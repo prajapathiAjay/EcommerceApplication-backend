e33e2526afeb28b5f4a10e2924b1055ee7eb467a
prajapathiAjay <ajayworking6309@gmail.com>
2025-08-18 22:07:38 +0530

    all about backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import  express from 'express'
-
+import productRouter from './src/product/controllers/product.routes.js';
+import  bodyParser from 'body-parser';
 const server= express();
+server.use(express.json());
+
 
 server.get("/",(req,res)=>{
     res.send("your server is running   successfully on port 3200")
 })
 
-server.listen(3200)
+
+// for all requests related to products redirect to product routes
+
+server.use("/api/products",productRouter)
+
+server.listen(3800)
 console.log("server is running on port 3200")
+
+
+
+
+
